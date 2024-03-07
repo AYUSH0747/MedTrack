20fd869520ae575decfe6c7468f5b8088e5ddd64
Ayush Ghogre <ayushghogre7@gmail.com>
2024-03-07 22:17:52 +0530

    Changed background

--- createReport.php
+++ createReport.php
@@ -18,6 +18,15 @@ $user = $_SESSION["user"];
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
     </head>
+    <style>
+
+        body{
+            background-image: url('https://img.freepik.com/free-vector/white-background-with-blue-tech-hexagon_1017-19366.jpg?w=900&t=st=1709404718~exp=1709405318~hmac=c2bc7121338492b34064448c232c4c465e17ba95d99800fde257d798890ee162');
+            background-size: cover;  This ensures the image covers the entire background 
+            background-repeat: no-repeat;
+        } 
+    </style>
+    
     <body>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <div>
