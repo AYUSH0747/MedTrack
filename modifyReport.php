a2d1195cec2920a880dc550326c1898065229acf
Ayush Ghogre <ayushghogre7@gmail.com>
2024-03-07 22:16:42 +0530

    Updated code

--- modifyReport.php
+++ modifyReport.php
@@ -13,6 +13,9 @@
             height: 100vh;
             margin: 0;
             background: linear-gradient(to bottom, #ffffff, #f2f5f7);
+            
+           
+        
         }
         .container {
             text-align: center;
